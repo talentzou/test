0bf82856c8225a7f23d346ec7eda0ad38afec15c
talentzou <115170057+talentzou@users.noreply.github.com>
2022-10-13 20:50:35 +0800

    Update x1.cpp

--- lessss/x1.cpp
+++ lessss/x1.cpp
@@ -3,7 +3,7 @@ int main()
 {
 	void fun(int b[3][3]);
 	int a[3][3]={{1,2,3},{4,5,6},{7,8,9}},i,j;
-	printf("转前:\n");
+	printf("脳陋脟掳:\n");
 	for(i=0;i<3;i++)
 		for(j=0;j<3;j++)
 		{
@@ -24,11 +24,13 @@ void fun(int b[3][3])
 			b[y][k]=b[k][y];
 			b[k][y]=t;
 		}
-		printf("转后:\n");
+		printf("脳陋潞贸:\n");
 for(y=0;y<3;y++)
 		for(k=0;k<3;k++)
 			printf("%4d",b[y][k]);
 		if(k==2)
 			printf("\n");
-}
+}/*
+
+*/
 
